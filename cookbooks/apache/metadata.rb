4dce09ef238165d405446ca064ba0e5668fc21c2
Kleber Leal <kleber.leal@gmail.com>
2019-01-05 13:21:51 +0100

    apache cookbook added.

---
+++ cookbooks/apache/metadata.rb
@@ -0,0 +1,22 @@
+name 'apache'
+maintainer 'Kleber Leal'
+maintainer_email 'kleber.leal@gmail.com'
+license 'GPL-2.0'
+description 'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version '0.1.0'
+chef_version '>= 12.14' if respond_to?(:chef_version)
+
+# The `issues_url` points to the location where issues for this cookbook are
+# tracked.  A `View Issues` link will be displayed on this cookbook's page when
+# uploaded to a Supermarket.
+#
+# issues_url 'https://github.com/<insert_org_here>/apache/issues'
+issues_url 'https://github.com/kaleal/chef-demo1/issues'
+
+# The `source_url` points to the development repository for this cookbook.  A
+# `View Source` link will be displayed on this cookbook's page when uploaded to
+# a Supermarket.
+#
+# source_url 'https://github.com/<insert_org_here>/apache'
+source_url 'https://github.com/kaleal/chef-demo1/apache'
